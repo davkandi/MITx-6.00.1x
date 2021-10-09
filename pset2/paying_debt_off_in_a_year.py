4b1ae6174ea87a1a4bae84a4a1b027e788c8517b
davkandi <davkandi@gmail.com>
2021-10-09 05:21:22 +0100

    October 2021

--- pset2/paying_debt_off_in_a_year.py
+++ pset2/paying_debt_off_in_a_year.py
@@ -1,14 +1,24 @@
-payment = 0
+balance = 1467.61
+annualInterestRate = 0.599
+
+lower = balance / 12
+upper = (balance * (1 + annualInterestRate / 12.0) ** 12) / 12.0
+payment = (lower + upper) / 2
 initbalance = balance
 
-while (balance > 0):
-    payment += 10
+while abs(balance) >= 0.01:
+    balance = initbalance
+    payment = (lower + upper) / 2
     for month in range(1, 13):
         unpaidbalance = balance - payment
         balance = unpaidbalance + (annualInterestRate / 12.0) * unpaidbalance
-    if (balance > 0):
-        balance = initbalance
+
+    if (balance <= 0.01):
+        upper = payment
+    elif (balance >= 0.01):
+        lower = payment
     else:
         break
-           
-print('lowest payment : ' + str(payment))
+    payment = (lower + upper) / 2
+
+print('lowest payment:', round(payment, 2))
